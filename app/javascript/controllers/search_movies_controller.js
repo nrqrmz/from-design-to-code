4ef9399478ea3b353094c0a814afe94e0ca2d2d6
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-30 02:56:55 -0500

    add stimulus controller, targets and actions for edit movie

--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -4,13 +4,12 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["form", "input", "list"]
 
-  connect() {
-    console.log(this.formTarget)
-    console.log(this.inputTarget)
-    console.log(this.listTarget)
-  }
-
   update() {
-    console.log(this.inputTarget.value)
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+    fetch(url, {headers: {"Accept": "text/plain"}})
+      .then(response => response.text())
+      .then((data) => {
+        this.listTarget.outerHTML = data
+      })
   }
 }
